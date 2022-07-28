4ec932f40419586df5e1cb771da95ac516e65547
ncrigo <ncrigobr@gmail.com>
2022-07-27 22:08:22 -0300

    commit inicial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import validator from './validator.js';
 
-console.log(validator);
+let b = window.document.getElementById('button');
+b.addEventListener('click', clicar);
+
+function clicar() {
+    let creditCardNumber = window.document.getElementById('numberInput');
+
+    let validou = validator.isValid(creditCardNumber);
+
+    let label = window.document.getElementById('numberInputLabel');
+
+    if (validou) {
+        creditCardNumber.style.border = '2px solid green'
+        creditCardNumber.value = validator.maskify(creditCardNumber);
+        label.innerText = 'Parabéns, pedido efetuado com sucesso!';
+        label.style.color = 'green';
+        label.style.fontWeight = 'bold';
+
+    } else {
+        creditCardNumber.style.border = '2px solid red';
+        label.innerText = 'Número inválido, tente outro cartão!';
+        label.style.color = 'red';
+        label.style.fontWeight = 'normal';
+    }
+
+    console.log(validou);
+}
+
+let d = window.document.getElementById('expiryInput');
+d.addEventListener('keypress', maskExpiry);
+
+function maskExpiry() {
+    let date = '';
+    date = date + d.value;
+    if (date.length == 2) {
+        date = date + '/';
+        d.value = date;
+    }
+}
