9f2714c1365d8b558c2bda3fe5c541d29d296c6c
ncrigo <ncrigobr@gmail.com>
2022-08-02 19:29:09 -0300

    finalizada validação do cartão, as mascaras e os ajustes da estilização

--- src/index.js
+++ src/index.js
@@ -1,18 +1,59 @@
 import validator from './validator.js';
 
+let creditCardNumber = window.document.getElementById('numberInput');
+
+creditCardNumber.addEventListener('keypress', justNumber);
+
+function justNumber(eventNumber) {
+    const keyCode = eventNumber.keyCode;
+    if (keyCode < 47 || keyCode > 58) {
+        eventNumber.preventDefault();
+    }
+}
+
+
+
+let expiry = window.document.getElementById('expiryInput');
+expiry.addEventListener('keypress', maskExpiry);
+
+function maskExpiry() {
+    let date = '';
+    date = date + expiry.value;
+    if (date.length == 2) {
+        date = date + '/';
+        expiry.value = date;
+    }
+}
+
+expiry.addEventListener('keypress', justNumber);
+
+
+
+let name = window.document.getElementById('nameInput');
+
+name.addEventListener('keypress', blockNumber);
+
+function blockNumber(eventName) {
+    const keyCode = eventName.keyCode;
+    if (keyCode > 33 && keyCode < 64) {
+        eventName.preventDefault();
+    }
+}
+
+
+
 let b = window.document.getElementById('button');
 b.addEventListener('click', clicar);
 
 function clicar() {
-    let creditCardNumber = window.document.getElementById('numberInput');
 
-    let validou = validator.isValid(creditCardNumber);
+    let validou = validator.isValid(creditCardNumber.value);
 
     let label = window.document.getElementById('numberInputLabel');
 
     if (validou) {
         creditCardNumber.style.border = '2px solid green'
-        creditCardNumber.value = validator.maskify(creditCardNumber);
+        creditCardNumber.value = validator.maskify(creditCardNumber.value);
         label.innerText = 'Parabéns, pedido efetuado com sucesso!';
         label.style.color = 'green';
         label.style.fontWeight = 'bold';
@@ -22,19 +63,5 @@ function clicar() {
         label.innerText = 'Número inválido, tente outro cartão!';
         label.style.color = 'red';
         label.style.fontWeight = 'normal';
-    }
-
-    console.log(validou);
-}
-
-let d = window.document.getElementById('expiryInput');
-d.addEventListener('keypress', maskExpiry);
-
-function maskExpiry() {
-    let date = '';
-    date = date + d.value;
-    if (date.length == 2) {
-        date = date + '/';
-        d.value = date;
     }
 }
--- src/validator.js
+++ src/validator.js
@@ -2,52 +2,23 @@ const validator = {
 
   isValid(creditCardNumber) {
 
-    let resultado = creditCardNumber.value.split('').map(Number);
+    let resultado = creditCardNumber.split('').map(Number);
 
-    resultado = resultado.reverse()
+    resultado = resultado.reverse();
 
-    resultado[1] = resultado[1] * 2;
-    resultado[3] = resultado[3] * 2;
-    resultado[5] = resultado[5] * 2;
-    resultado[7] = resultado[7] * 2;
-    resultado[9] = resultado[9] * 2;
-    resultado[11] = resultado[11] * 2;
-    resultado[13] = resultado[13] * 2;
-    resultado[15] = resultado[15] * 2;
-
-    if (resultado[1] > 9) {
-      resultado[1] = resultado[1] - 9
+    let element = 1;
+    while (element < resultado.length) {
+      resultado[element] = resultado[element] * 2;
+      if (resultado[element] > 9) {
+        resultado[element] = resultado[element] - 9
+      }
+      element = element + 2;
     }
 
-    if (resultado[3] > 9) {
-      resultado[3] = resultado[3] - 9
+    let sum = 0;
+    for (element = 0; element < resultado.length; element++) {
+      sum = sum + resultado[element];
     }
-
-    if (resultado[5] > 9) {
-      resultado[5] = resultado[5] - 9
-    }
-
-    if (resultado[7] > 9) {
-      resultado[7] = resultado[7] - 9
-    }
-
-    if (resultado[9] > 9) {
-      resultado[9] = resultado[9] - 9
-    }
-
-    if (resultado[11] > 9) {
-      resultado[11] = resultado[11] - 9
-    }
-
-    if (resultado[13] > 9) {
-      resultado[13] = resultado[13] - 9
-    }
-
-    if (resultado[15] > 9) {
-      resultado[15] = resultado[15] - 9
-    }
-
-    let sum = resultado[0] + resultado[1] + resultado[2] + resultado[3] + resultado[4] + resultado[5] + resultado[6] + resultado[7] + resultado[8] + resultado[9] + resultado[10] + resultado[11] + resultado[12] + resultado[13] + resultado[14] + resultado[15];
 
     if (sum % 10 === 0) {
       return true;
@@ -58,11 +29,11 @@ const validator = {
   },
 
   maskify(creditCardNumber) {
-    let cardLength = creditCardNumber.value.length;
-    let mask = creditCardNumber.value.substring(0, cardLength - 4);
+    let cardLength = creditCardNumber.length;
+    let mask = creditCardNumber.substring(0, cardLength - 4);
     mask = mask.replace(/./g, '#');
 
-    let text = creditCardNumber.value.substring(cardLength - 4);
+    let text = creditCardNumber.substring(cardLength - 4);
     let newCard = mask + text;
     return newCard;
   }
